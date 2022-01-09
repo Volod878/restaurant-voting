e7050c366c7b0bb8d8b49189942b63b5f6228614
Volod878 <volod1988@gmail.com>
2022-01-09 20:43:48 +0700

    initial

---
+++ src/test/java/ru/volod878/restaurantvoting/RestaurantVotingApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.volod878.restaurantvoting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantVotingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
